c647d03bd72f4138e2f4007f3cbee1ab6cfff7de
alstjd223 <13hj45@gmail.com>
2024-11-22 14:31:38 +0900

    에

--- index.js
+++ index.js
@@ -54,6 +54,6 @@ app.post('/log', (req, res) => {
     res.sendStatus(200);
 });
 
-app.listen(4000, () => {
-    console.log("Server is running on port 4000");
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
 });
